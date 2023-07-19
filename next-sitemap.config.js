01b253d745ef0ef5b146b85148f14af903f4f2ea
Krish120003 <krish120003@gmail.com>
2023-07-19 03:25:43 -0400

    fix: sitemap support for vercel

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.CF_PAGES_URL || "http://localhost:3000",
+  siteUrl: (process.env.NODE_ENV === "production" && process.env.DEPLOYMENT_URL)"http://localhost:3000",
   generateRobotsTxt: true, // (optional)
   outDir: "out/",
   // ...other options
